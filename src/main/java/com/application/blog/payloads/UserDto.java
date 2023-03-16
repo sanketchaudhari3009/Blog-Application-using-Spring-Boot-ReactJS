1c63b8ef13bec652865e8b348cdc79732468798c
Sanket Chaudhari <78204805+sanketchaudhari3009@users.noreply.github.com>
2023-03-16 23:33:49 +0530

    Validating data using Bean Validator

--- src/main/java/com/application/blog/payloads/UserDto.java
+++ src/main/java/com/application/blog/payloads/UserDto.java
@@ -1,5 +1,11 @@
 package com.application.blog.payloads;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,9 +16,19 @@ import lombok.Setter;
 public class UserDto {
 
 	private int id;
+	
+	@NotEmpty
+	@Size(min = 4, message = "Username must be min of 4char")
 	private String name;
+	
+	@Email(message = "Email address is not valid")
 	private String email;
+	
+	@NotEmpty
+	@Size(min = 3, max = 10, message = "Password must be between 3 to 10 characters")
 	private String password;
+	
+	@NotEmpty
 	private String about;
 
 	
