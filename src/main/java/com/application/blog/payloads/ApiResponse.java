21288fd85d847a8f7051d615b8ad0944bc68ac64
Sanket Chaudhari <78204805+sanketchaudhari3009@users.noreply.github.com>
2023-03-16 22:23:16 +0530

    User Controller Get, Put, Post Mapping

---
+++ src/main/java/com/application/blog/payloads/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.application.blog.payloads;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+
+	private String message;
+	private boolean success;
+}
