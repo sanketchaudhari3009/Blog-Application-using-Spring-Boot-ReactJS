af1becc588fa919343b519efedf23fc6b767a585
Sanket Chaudhari <78204805+sanketchaudhari3009@users.noreply.github.com>
2023-03-18 10:46:23 +0530

    Creating API for updating and deleting post

--- src/main/java/com/application/blog/controllers/PostController.java
+++ src/main/java/com/application/blog/controllers/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.application.blog.payloads.ApiResponse;
 import com.application.blog.payloads.PostDto;
 import com.application.blog.payloads.PostResponse;
 import com.application.blog.services.PostService;
@@ -50,17 +51,17 @@ public class PostController {
 	}
 	
 	// get all posts
-
-	@GetMapping("/posts")
-	public ResponseEntity<PostResponse> getAllPost(
-			@RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
-			@RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
-			@RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
-			@RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir) {
-
-		PostResponse postResponse = this.postService.getAllPost(pageNumber, pageSize, sortBy, sortDir);
-		return new ResponseEntity<PostResponse>(postResponse, HttpStatus.OK);
-	}
+//
+//	@GetMapping("/posts")
+//	public ResponseEntity<PostResponse> getAllPost(
+//			@RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+//			@RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+//			@RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+//			@RequestParam(value = "sortDir", defaultValue = AppConstants.SORT_DIR, required = false) String sortDir) {
+//
+//		PostResponse postResponse = this.postService.getAllPost(pageNumber, pageSize, sortBy, sortDir);
+//		return new ResponseEntity<PostResponse>(postResponse, HttpStatus.OK);
+//	}
 
 	// get post details by id
 
@@ -72,5 +73,23 @@ public class PostController {
 
 	}
 
+	// delete post
+	@DeleteMapping("/posts/{postId}")
+	public ApiResponse deletePost(@PathVariable Integer postId) {
+		
+		this.postService.deletePost(postId);
+		return new ApiResponse("Post is successfully deleted !!", true);
+	}
+	
+	// update post
+
+	@PutMapping("/posts/{postId}")
+	public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable Integer postId) {
+
+		PostDto updatePost = this.postService.updatePost(postDto, postId);
+		return new ResponseEntity<PostDto>(updatePost, HttpStatus.OK);
+
+	}
+	
 
 }
--- src/main/java/com/application/blog/services/impl/PostServiceImpl.java
+++ src/main/java/com/application/blog/services/impl/PostServiceImpl.java
@@ -56,42 +56,57 @@ public class PostServiceImpl implements PostService {
 
 	@Override
 	public PostDto updatePost(PostDto postDto, Integer postId) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Post post = this.postRepo.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post ", "post id", postId));
+
+        Category category = this.categoryRepo.findById(postDto.getCategory().getCategoryId()).get();
+
+        post.setTitle(postDto.getTitle());
+        post.setContent(postDto.getContent());
+        post.setImageName(postDto.getImageName());
+        post.setCategory(category);
+
+
+        Post updatedPost = this.postRepo.save(post);
+        return this.modelMapper.map(updatedPost, PostDto.class);
 	}
 
 	@Override
 	public void deletePost(Integer postId) {
-		// TODO Auto-generated method stub
+		
+		Post post = this.postRepo.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post ", "post id", postId));
 
+        this.postRepo.delete(post);
 	}
 	
-	@Override
-    public PostResponse getAllPost(Integer pageNumber, Integer pageSize, String sortBy, String sortDir) {
-
-        Sort sort = (sortDir.equalsIgnoreCase("asc")) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
-
-        Pageable p = PageRequest.of(pageNumber, pageSize, sort);
-
-        Page<Post> pagePost = this.postRepo.findAll(p);
-
-        List<Post> allPosts = pagePost.getContent();
-
-        List<PostDto> postDtos = allPosts.stream().map((post) -> this.modelMapper.map(post, PostDto.class))
-                .collect(Collectors.toList());
-
-        PostResponse postResponse = new PostResponse();
-
-        postResponse.setContent(postDtos);
-        postResponse.setPageNumber(pagePost.getNumber());
-        postResponse.setPageSize(pagePost.getSize());
-        postResponse.setTotalElements(pagePost.getTotalElements());
-
-        postResponse.setTotalPages(pagePost.getTotalPages());
-        postResponse.setLastPage(pagePost.isLast());
-
-        return postResponse;
-    }
+//	@Override
+//    public PostResponse getAllPost(Integer pageNumber, Integer pageSize, String sortBy, String sortDir) {
+//
+//        Sort sort = (sortDir.equalsIgnoreCase("asc")) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+//
+//        Pageable p = PageRequest.of(pageNumber, pageSize, sort);
+//
+//        Page<Post> pagePost = this.postRepo.findAll(p);
+//
+//        List<Post> allPosts = pagePost.getContent();
+//
+//        List<PostDto> postDtos = allPosts.stream().map((post) -> this.modelMapper.map(post, PostDto.class))
+//                .collect(Collectors.toList());
+//
+//        PostResponse postResponse = new PostResponse();
+//
+//        postResponse.setContent(postDtos);
+//        postResponse.setPageNumber(pagePost.getNumber());
+//        postResponse.setPageSize(pagePost.getSize());
+//        postResponse.setTotalElements(pagePost.getTotalElements());
+//
+//        postResponse.setTotalPages(pagePost.getTotalPages());
+//        postResponse.setLastPage(pagePost.isLast());
+//
+//        return postResponse;
+//    }
 
 	@Override
 	public PostDto getPostById(Integer postId) {
